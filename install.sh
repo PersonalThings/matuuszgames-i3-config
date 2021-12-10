cc6a84ee629600f6f8160a7f3534b35a3fa106f4
MatusModder <76532262+MatusModder@users.noreply.github.com>
2021-12-10 09:53:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
+if cat /etc/os-release| grep Debian,Ubuntu then
+sudo apt install git
+git clone https://github.com/DebianFanboy/debian-i3gaps-from-scratch.git
+cd debian-i3gaps-from-scratch
+chmod +x run.sh
+./run.sh
+elif cat /etc/os-release | grep Arch,Manjaro,Arco,Endeavour,MatuusOS then
+sudo pacman -S --needed lxsession i3 lxdm xorg-xserver papirus-icon-theme arc-gtk-theme chromium
 rsync config .config
